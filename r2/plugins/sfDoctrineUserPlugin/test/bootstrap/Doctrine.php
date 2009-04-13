d730eb014658d0e78430fe98c48ec94b9fb96e27
jaspertomas <jaspertomas@66c21fb8-2428-11de-a78b-a7aca27b2395>
2009-04-13 07:14:14 +0000

    delete r2 added by mistake

--- r2/plugins/sfDoctrineUserPlugin/test/bootstrap/Doctrine.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include(dirname(__FILE__).'/unit.php');
-
-new sfDatabaseManager($configuration);
-$con = Doctrine_Manager::getInstance()->getCurrentConnection();
-Doctrine::loadData($configuration->getRootDir().'/test/fixtures/project/data/fixtures', false);
